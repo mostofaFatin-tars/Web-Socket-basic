e82f51b98a2153d7ed621b76c6d77ad8542fa374
mostofaFatin-tars <mostofafatin.tars@gmail.com>
2024-05-02 11:11:06 +0600

    first commit

---
+++ Client/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1>React Multiplayer dashboard</h1>
+    </>
+  )
+}
+
+export default App
---
+++ Server/server.js
@@ -0,0 +1,20 @@
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const httpServer = createServer();
+
+
+const socket = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "DELETE", "PUT"], // Allow only specific methods
+    allowedHeaders: ["Authorization"], // Allow only specific headers
+  },
+});
+
+socket.on("connection", (socket) => {
+  console.log("Connected", socket);
+});
+
+httpServer.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
